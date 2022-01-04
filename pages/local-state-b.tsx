ced11eaa60054b90a433ee561ade487500b87f87
okamoto-tosei <tosei393939@gmail.com>
2022-01-04 09:52:26 +0900

    gql設定

---
+++ pages/local-state-b.tsx
@@ -0,0 +1,13 @@
+import { VFC } from 'react'
+import { LocalStateB } from '../components/LocalStateB'
+import { Layout } from '../components/Layout'
+
+const LocalStatePageB: VFC = () => {
+  return (
+    <Layout title="Local state B">
+      <LocalStateB />
+    </Layout>
+  )
+}
+
+export default LocalStatePageB
